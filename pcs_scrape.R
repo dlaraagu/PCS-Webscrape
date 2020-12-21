4644f95937619abb8bb48ace8403cd1087bc58df
dlaraagu <41123726+dlaraagu@users.noreply.github.com>
2020-12-21 18:04:58 -0500

    Update pcs_scrape.R

--- pcs_scrape.R
+++ pcs_scrape.R
@@ -6,7 +6,7 @@ library(rvest)
 years <- list(seq(from = 2000, to = 2020))
 
 #Specify the race.
-r <- "dauphine"
+r <- "tour-de-france"
 
 #Create an empty dataset to append to. 
 df <- data.frame(Rnk = character(), Rider = character(), Time = character(), stringsAsFactors = FALSE)
@@ -23,13 +23,19 @@ for (y in years[[1]]) {
   df_sub <- html %>% html_nodes(tloc) %>% .[[2]] %>% html_table(fill = TRUE)
   
   #Clean the data before appending.
-  df_sub$Rnk   <- with(df_sub, ifelse(is.na(df_sub$Rnk), shift(df_sub$Rnk, type = 'lag') + 1, df_sub$Rnk))
+  if ("Prev" %in% names(df_sub)) {
+    df_sub$Rnk <- ifelse(is.na(df_sub$Rnk), df_sub$Prev, df_sub$Rnk)
+  }
+  df_sub$Rnk   <- ifelse(is.na(df_sub$Rnk), shift(df_sub$Rnk, type = 'lag') + 1, df_sub$Rnk)
   df_sub$Rider <- substr(df_sub$Rider, 1, nchar(df_sub$Rider) - nchar(df_sub$Team))
   df_sub$Time  <- substr(df_sub$Time, 1, ceiling(nchar(df_sub$Time)/2))
-  df_sub$Year <- y
+  df_sub$Year  <- y
   
   #Format the time field.
-  df_sub$Time <- with(df_sub, ifelse(grepl(",,", df_sub$Time), shift(df_sub$Time, type = 'lag'), df_sub$Time))
+  while (any(grepl(",,", df_sub$Time))) { 
+    df_sub$Time <- with(df_sub, ifelse(grepl(",,", df_sub$Time), shift(df_sub$Time, type = 'lag'), df_sub$Time))
+  }
+  
   df_sub$Time <- with(df_sub, ifelse(nchar(df_sub$Time) == 4, paste("00:0", df_sub$Time, sep = ""), df_sub$Time))
   df_sub$Time <- with(df_sub, ifelse(nchar(df_sub$Time) == 5, paste("00:", df_sub$Time, sep = ""), df_sub$Time))
   df_sub$Time <- with(df_sub, ifelse(nchar(df_sub$Time) == 7, paste("0", df_sub$Time, sep = ""), df_sub$Time))
