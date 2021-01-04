cd368427be2732ff0270dd4c9be54c3fe79953ef
dlaraagu <41123726+dlaraagu@users.noreply.github.com>
2021-01-04 14:49:57 -0500

    Update pcs_scrape.R

--- pcs_scrape.R
+++ pcs_scrape.R
@@ -20,13 +20,16 @@ for (y in years[[1]]) {
   
   #Specify the location of the results table.
   tloc <- "body > div.wrapper > div.content > div.res-left > div.resultCont > table"
-  df_sub <- html %>% html_nodes(tloc) %>% .[[2]] %>% html_table(fill = TRUE)
+  df_sub <- html %>% html_nodes(tloc) %>% .[[ind]] %>% html_table(fill = TRUE)
   
   #Clean the data before appending.
-  df_sub$Rank <- rownames(df_sub)
-  df_sub$Rider <- substr(df_sub$Rider, 1, nchar(df_sub$Rider) - nchar(df_sub$Team))
+  df_sub$Rider <- ifelse(is.na(df_sub$Team), df_sub$Rider, mapply(gsub, pattern = df_sub$Team, replacement = "", x = df_sub$Rider))
   df_sub$Time  <- substr(df_sub$Time, 1, ceiling(nchar(df_sub$Time) / 2))
+  df_sub$Rank  <- as.numeric(rownames(df_sub))
   df_sub$Year  <- y
+  
+  #Remove riders who DNFed.
+  df_sub <- subset(df_sub, is.na(df_sub$Rnk) | (df_sub$Rnk != "DNF" & df_sub$Rnk != "OTL" & df_sub$Rnk != "DNS" & df_sub$Rnk != "DSQ"))
   
   #Format the time field.
   while (any(grepl(",,", df_sub$Time))) { 
@@ -36,14 +39,16 @@ for (y in years[[1]]) {
   df_sub$Time <- with(df_sub, ifelse(nchar(df_sub$Time) == 4, paste("00:0", df_sub$Time, sep = ""), df_sub$Time))
   df_sub$Time <- with(df_sub, ifelse(nchar(df_sub$Time) == 5, paste("00:", df_sub$Time, sep = ""), df_sub$Time))
   df_sub$Time <- with(df_sub, ifelse(nchar(df_sub$Time) == 7, paste("0", df_sub$Time, sep = ""), df_sub$Time))
-  df_sub$Time <- as.numeric(substr(df_sub$Time, 1, 2)) + as.numeric(substr(df_sub$Time, 4, 5)) / 60 + 
-                 as.numeric(substr(df_sub$Time, 7, 8)) / 3600
-  df_sub$Time[df_sub$Rank != 1] <- df_sub$Time[df_sub$Rank != 1] + df_sub$Time[1]
+  
+  df_sub$hrs  <- as.numeric(sapply(strsplit(df_sub$Time, ":"), "[", 1))
+  df_sub$min  <- as.numeric(sapply(strsplit(df_sub$Time, ":"), "[", 2))
+  df_sub$sec  <- as.numeric(sapply(strsplit(df_sub$Time, ":"), "[", 3))
+  df_sub$Time <- df_sub$hrs + (df_sub$min / 60) + (df_sub$sec / 3600)
+  df_sub$Time <- ifelse(df_sub$Time < df_sub$Time[1], df_sub$Time + df_sub$Time[1], df_sub$Time)
   
   #Select the relevant data fields. 
   df_sub <- df_sub[c("Year", "Rank", "Rider", "Age", "Team", "Time")]
   
   #Append to the empty dataset.
   df <- rbind(df, df_sub)
-  
 }
